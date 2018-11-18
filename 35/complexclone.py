3fd71159f590e5d443707f0a41e3f71de67f20b8
lyyyuna <lyyyuna@gmail.com>
2018-11-18 16:28:13 +0800

    update

--- 35/complexclone.py
+++ 35/complexclone.py
@@ -13,3 +13,30 @@ def cloneNodes(pHead):
         pNode.next = cloneNode
         cloneNode.value = pNode.value
         pNode = cloneNode.next
+
+
+def connectSiblingNodes(pHead):
+    pNode = pHead
+    while pNode:
+        pClone = pNode.next
+        if pNode.sibling:
+            pClone.sibling = pNode.sibling.next
+        pNode = pClone.next
+
+
+def reconnectNodes(pHead):
+    pNode = pHead
+    pClone = pHead.next
+    pCloneHead = pClone
+
+    pNode.next = pClone.next
+    pNode = pNode.next
+
+    while pNode:
+        pClone.next = pNode.next
+        pClone = pClone.next
+
+        pNode.next = pClone.next
+        pNode = pNode.next
+
+    return pCloneHead
